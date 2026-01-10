a41161f09cc69c6904f211feb0968344fbebe8ee
kingjohnreese-web <kingjohnreese@gmail.com>
2026-01-10 21:22:22 +0300

    Add files via upload

---
+++ koyeb.js
@@ -0,0 +1,17 @@
+FROM fedora:37
+
+RUN sudo dnf -y update &&\
+    sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&\
+    sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
+    sudo dnf clean all -y
+
+RUN git clone https://github.com/kingjohnreese-web/MADARA-UCHIHA.git
+
+
+WORKDIR /root/DAVE-X
+
+COPY ./root/DAVE-X
+
+RUN yarn install
+
+CMD ["node", "index.js"]
